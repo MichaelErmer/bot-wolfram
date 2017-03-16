4753d9983991afde014f8d6766d7ffd72c252d9d
Michael Ermer <michael.ermer@ermer.de>
2017-03-16 21:19:47 +0100

    init

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var wolfram = require('wolfram').createClient(process.env.WOLFRAM_APPID);
 var Module = function (bot) {
   this.bot = bot;
   this.name = "Wofram Library";
-  this.version = "0.1";
+  this.version = "0.2";
   this.help = function () {
     // RETURN HELP STRING FOR YOUR COMMANDS or AT LEAST YOUR COMMANDS Object.keys(this.commands)
     return {
