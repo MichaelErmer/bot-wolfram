0a971f857fc36c38f26d6d5987879566b04a2102
Michael Ermer <michael.ermer@ermer.de>
2017-03-16 17:52:19 +0100

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+var _ = require('lodash');
+var wolfram = require('wolfram').createClient(provess.env.WOLFRAM_APPID);
+
+var Module = function (bot) {
+  this.bot = bot;
+  this.name = "Wofram Library";
+  this.version = "0.1";
+  this.help = function () {
+    // RETURN HELP STRING FOR YOUR COMMANDS or AT LEAST YOUR COMMANDS Object.keys(this.commands)
+    return {
+      "ping": "Pings a host 3 times",
+    };
+  };
+  this.commands = {};
+
+  this.commands.wolfram = function(channel, args, user) {
+    var args = args.split(" ");
+    if (!args.length || !args[0]) bot.postMessage(channel, "Invalid Request");
+    wolfram.query(args[0], function(error, result) {
+      if (error) console.log(error);
+      bot.postMessage(channel, "```"+JSON.stringify(result, null, 2)+"```");
+    });
+  };
+
+};
+
+Module.prototype.toString = function() {
+  return this.name;
+};
+
+
+var exports = module.exports = Module;
